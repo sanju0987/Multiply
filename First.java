6b2cc8c5da5b5f13165a56fe10214161856872d2
admin <ssaannjjuu098@gmail.com>
2022-03-09 17:10:15 +0530

    Newly added

--- First.java
+++ First.java
@@ -1,2 +1,14 @@
-Hello!!!!
-this is first repository!!!
+package repo;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8;
+		int b = 4;
+		int c = a * b;
+		System.out.println(c);
+
+	}
+
+}
